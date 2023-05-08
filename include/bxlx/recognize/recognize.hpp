e3c8f610b3a61ee1a5fcd125ae949ad2101e947d
schaumb <schaumb@gmail.com>
2023-05-08 13:43:04 +0200

    revert HEAD~3

--- include/bxlx/recognize/recognize.hpp
+++ include/bxlx/recognize/recognize.hpp
@@ -515,7 +515,7 @@ namespace state_machine {
   struct transition {
     template <class T, class Props = properties::empty_t>
     constexpr static auto valid() {
-      if constexpr (!valid_type<T>().value) {
+      if constexpr (!valid_type<T>()) {
         return TypeFilter::template valid<T>();
       } else if constexpr (!valid_conditions<T, Props>().value) {
         return Conditions->template valid<T, Props>();
@@ -531,7 +531,7 @@ namespace state_machine {
 
     template <class T, class Props = properties::empty_t>
     constexpr static auto valid_conditions() {
-      if constexpr (valid_type<T>().value) {
+      if constexpr (valid_type<T>()) {
         return Conditions->template valid<T, Props>();
       } else {
         return valid_type<T>();
